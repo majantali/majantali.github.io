f6bcc7b1a14a14ed6a9d16b3eccb5e51ff5b6abd
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2020-04-05 12:40:54 +0530

    exclude images from epub

--- bnarchive/generate-epub.sh
+++ bnarchive/generate-epub.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+
+## Want to exclude <img> tags: doing this crudely for now, but should
+## ideally write a pandoc filter? There is unfortunately no option to
+## remove all images when generating epubs.
+
 for MDFILE in *_*.md
 do
     EPUBFILE=${MDFILE/.md/.epub}
+    TMPFILE=${MDFILE/.md/.tmp}
     echo "Generating ${EPUBFILE}"
-    pandoc -o ${EPUBFILE} ${MDFILE}
+    grep -v "^<img" ${MDFILE} > ${TMPFILE}
+    pandoc -f markdown -o ${EPUBFILE} ${TMPFILE}
+    rm ${TMPFILE}
 done
 
 
